896da2dd0306062d38040ac9e32d77e2e2693ad1
Châu Thái Nhân <ctnhan044@gmail.com>
2016-08-31 15:59:58 +0700

    bug fix

--- api2captcha.php
+++ api2captcha.php
@@ -4,16 +4,59 @@ class Api2Captcha {
 
     public $requestTimeout = 5;
     public $maxTimeout = 60;
+
+    /**
+     * 0 = 1 word (default value)
+     * 1 = CAPTCHA contains 2 words
+     * @var int 
+     */
     public $isPhrase = 0;
+
+    /**
+     * 0 = not case sensitive (default value)
+     * 1 = case sensitive 
+     * @var int 
+     */
     public $isRegSense = 0;
+
+    /**
+     * 0 = not specified (default value)
+     * 1 = numeric CAPTCHA
+     * 2 = letters CAPTCHA
+     * 3 = either numeric or letters. 
+     * @var int
+     */
     public $isNumeric = 0;
+
+    /**
+     * 0 = not specified (default value)
+     * 1..20 = minimal number of symbols in the CAPTCHA text
+     * @var int 
+     */
     public $minLen = 0;
+
+    /**
+     * 0 = not specified (default value)
+     * 1..20 = maximal number of symbols in the CAPTCHA text
+     * @var int
+     */
     public $maxLen = 0;
+
+    /**
+     * 0 = not specified (default value)
+     * 1 = Cyrillic CAPTCHA
+     * 2 = Latin CAPTCHA
+     * @var int 
+     */
     public $language = 0;
-    private $domain = 'http://rucaptcha.com';
     protected $apiKey;
     protected $result;
     protected $error;
+    private $captcha_id;
+
+    const API_IN_URL = 'http://2captcha.com/in.php';
+    const API_RES_URL = 'http://2captcha.com/res.php';
+
     private $errors = array(
         'ERROR_NO_SLOT_AVAILABLE' => 'No free workers at the moment, try again later or promote your maximum bid here',
         'ERROR_ZERO_CAPTCHA_FILESIZE' => 'CAPTCHA size that you load less than 100 bytes',
@@ -29,7 +72,6 @@ class Api2Captcha {
         'ERROR_WRONG_ID_FORMAT' => 'Invalid format ID captcha. ID must contain only digits',
         'ERROR_WRONG_FILE_EXTENSION' => 'Your captcha is incorrect extension, allowed extensions jpg, jpeg, gif, png',
     );
-    private $captcha_id;
 
     public function __construct($apiKey, $minLength = 0, $maxLength = 3) {
         if (empty($apiKey)) {
@@ -42,8 +84,12 @@ class Api2Captcha {
     }
 
     public function decode($filename) {
+        if (!file_exists($filename)) {
+            die('Captcha file not exist');
+        }
         try {
-            $postData = array('method' => 'post',
+            $postData = array(
+                'method' => 'post',
                 'key' => $this->apiKey,
                 'file' => '@' . $filename,
                 'phrase' => $this->isPhrase,
@@ -52,54 +98,66 @@ class Api2Captcha {
                 'min_len' => $this->minLen,
                 'max_len' => $this->maxLen,
                 'language' => $this->language,
-                'soft_id' => 882
+                'soft_id' => 2040009
             );
 
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, "{$this->domain}/in.php");
-            if (version_compare(PHP_VERSION, '5.5.0') >= 0 && version_compare(PHP_VERSION, '7.0') < 0) {
-                curl_setopt($ch, CURLOPT_SAFE_UPLOAD, false);
-            }
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_TIMEOUT, 60);
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
-            $result = curl_exec($ch);
-            if (curl_errno($ch)) {
-                throw new Exception("CURL error: " . curl_error($ch));
-            }
-            curl_close($ch);
+            $result = $this->excutePost(self::API_IN_URL, $postData);
             $this->setError($result);
-            list(, $this->captcha_id) = explode("|", $result);
-            $waitTime = 0;
-            sleep($this->requestTimeout);
-
-            while (true) {
-                $result = file_get_contents("{$this->domain}/res.php?key={$this->apiKey}&action=get&id={$this->captcha_id}");
-                $this->setError($result);
-                if ($result == "CAPCHA_NOT_READY") {
-                    $waitTime += $this->requestTimeout;
-                    if ($waitTime > $this->maxTimeout) {
-                        break;
-                    }
-                    sleep($this->requestTimeout);
-                } else {
-                    $ex = explode('|', $result);
-                    if (trim($ex[0]) == 'OK') {
-                        $this->result = trim($ex[1]);
-                        return true;
-                    }
-                }
-            }
-            throw new Exception('Timeout');
+            $this->captcha_id = end(explode("|", $result));
+            return $this->getText();
         } catch (Exception $e) {
             $this->error = $e->getMessage();
             return false;
         }
     }
 
+    protected function excutePost($url, $data) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        if (version_compare(PHP_VERSION, '5.5.0') >= 0 && version_compare(PHP_VERSION, '7.0') < 0) {
+            curl_setopt($ch, CURLOPT_SAFE_UPLOAD, false);
+        }
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        $result = curl_exec($ch);
+        if (curl_errno($ch)) {
+            throw new Exception("CURL error: " . curl_error($ch));
+        }
+        curl_close($ch);
+        return $result;
+    }
+
+    protected function getText() {
+        $waitTime = 0;
+        sleep($this->requestTimeout);
+        while (true) {
+            $result = file_get_contents(self::API_RES_URL . "?key={$this->apiKey}&action=get&id={$this->captcha_id}");
+            $this->setError($result);
+            if ($result == "CAPCHA_NOT_READY") {
+                sleep($this->requestTimeout);
+            } else {
+                return $this->checkText($result);
+            }
+            $waitTime += $this->requestTimeout;
+            if ($waitTime > $this->maxTimeout) {
+                return false;
+            }
+        }
+    }
+
+    protected function checkText($result) {
+        $ex = explode('|', $result);
+        if (trim($ex[0]) == 'OK') {
+            $this->result = trim($ex[1]);
+            return true;
+        }
+        return false;
+    }
+
     public function inccorect() {
-        file_get_contents("{$this->domain}/res.php?key={$this->apiKey}&action=reportbad&id={$this->captcha_id}");
+        file_get_contents(self::API_RES_URL . "?key={$this->apiKey}&action=reportbad&id={$this->captcha_id}");
     }
 
     public function error() {
@@ -121,4 +179,3 @@ class Api2Captcha {
     }
 
 }
-?>
